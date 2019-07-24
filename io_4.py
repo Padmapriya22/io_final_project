65c0bb1563562ef64a73941fc35de2c5a002102f
Padmapriya22 <51435231+Padmapriya22@users.noreply.github.com>
2019-07-24 15:16:26 +0530

    Delete io_4.py

--- io_4.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Please Enter any Number: "))
-Sum = 0
-
-while(number > 0):
-    Reminder = number % 10
-    Sum = Sum + Reminder
-    number = number //10
-
-print("Sum of the digits of Given Number =",Sum)
